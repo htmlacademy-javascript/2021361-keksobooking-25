14b1a2b00c65d7bb34decdecc3d1558c0b032191
Перцев Денис <thech@yandex.ru>
2022-02-24 23:41:47 +0300

    исправил синтаксические ошибки

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ function getRandomIntInclusive(min, max) {
 }
 
 getRandomIntInclusive(0, 1);
+
 /**
  * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
  * @param {number} min      - Первое положительное число, включая "0"
@@ -21,13 +22,14 @@ getRandomIntInclusive(0, 1);
  * @param {number} digits   - количество знаков после запятой, значения от 0 до 20 включительно
  * {@link https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random}
  */
-function (min, max, digits) {
+function getRandomFloatInclusive(min, max, digits) {
   min = Number(min.toFixed(digits));
-  max = Number(maxgetRandomFloatInclusive.toFixed(digits));
+  max = Number(max.toFixed(digits));
   if (min < 0 || max < 0 || min >= max) {
     return -1;
   }
   const result = Math.random() * (max - min + 1 / 10 ** (digits + 1)) + min;
   return Number(result.toFixed(digits));
 }
+
 getRandomFloatInclusive(0, 1, 1);
