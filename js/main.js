6eb8674f1e5f683a0324451d62c3a72b212173f5
Перцев Денис <thech@yandex.ru>
2022-02-24 23:35:47 +0300

    добавил вызовы функций для линтера

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+getRandomIntInclusive(0, 1);
 /**
  * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
  * @param {number} min      - Первое положительное число, включая "0"
@@ -20,12 +21,13 @@ function getRandomIntInclusive(min, max) {
  * @param {number} digits   - количество знаков после запятой, значения от 0 до 20 включительно
  * {@link https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random}
  */
-function getRandomFloatInclusive(min, max, digits) {
+function (min, max, digits) {
   min = Number(min.toFixed(digits));
-  max = Number(max.toFixed(digits));
+  max = Number(maxgetRandomFloatInclusive.toFixed(digits));
   if (min < 0 || max < 0 || min >= max) {
     return -1;
   }
   const result = Math.random() * (max - min + 1 / 10 ** (digits + 1)) + min;
   return Number(result.toFixed(digits));
 }
+getRandomFloatInclusive(0, 1, 1);
