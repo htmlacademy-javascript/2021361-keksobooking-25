bb4c18c8c3fab4702b0136acf1ddbc3d1f677352
Перцев Денис <thech@yandex.ru>
2022-02-27 16:30:28 +0300

    рефакторинг функций getRandomInteger, getRandomFloat

--- js/main.js
+++ js/main.js
@@ -1,35 +1,59 @@
 /**
  * Функция возвращает случайное целого число в заданном интервале, включительно
- * @param {number} min - Первое положительное число, включая "0"
- * @param {number} max - Второе положительное число, больше первого
- * {@link https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random}
+ * @param {number} min - первое положительное число, включая "0"
+ * @param {number} max - второе положительное число, больше первого
+ * @param {boolean} includesMax - если true, результат может включать значение 'max'
  */
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if (min < 0 || max < 0 || min >= max) {
-    return -1;
+function getRandomInteger(min, max, includesMax = false) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    throw new Error('один из параметров не является целым числом');
   }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (min < 0 || max <= min) {
+    throw new Error(
+      'параметры не удовлетворяют условиям: min, max > 0, max > min'
+    );
+  }
+  let result;
+  if (includesMax) {
+    result = min + Math.floor(Math.random() * (max - min + 1));
+  } else {
+    result = min + Math.floor(Math.random() * (max - min));
+  }
+  return result;
 }
 
-getRandomIntInclusive(0, 1);
+getRandomInteger(1, 3, true);
 
 /**
  * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
- * @param {number} min      - Первое положительное число, включая "0"
- * @param {number} max      - Второе положительное число, больше первого
- * @param {number} digits   - количество знаков после запятой, значения от 0 до 20 включительно
- * {@link https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random}
+ * @param {number} min        - первое положительное число, включая 0
+ * @param {number} max        - второе положительное число, больше первого
+ * @param {number} precision  - количество знаков после запятой, целое число >= 1
  */
-function getRandomFloatInclusive(min, max, digits) {
-  min = Number(min.toFixed(digits));
-  max = Number(max.toFixed(digits));
-  if (min < 0 || max < 0 || min >= max) {
-    return -1;
+function getRandomFloat(min, max, precision) {
+  if (
+    typeof min !== 'number' ||
+    typeof max !== 'number' ||
+    typeof precision !== 'number' ||
+    isNaN(min) ||
+    isNaN(max) ||
+    isNaN(precision)
+  ) {
+    throw new Error('один или несколько паоаметров не являются числами');
   }
-  const result = Math.random() * (max - min + 1 / 10 ** (digits + 1)) + min;
-  return Number(result.toFixed(digits));
+  if (min < 0 || max <= min || precision < 1 || !Number.isInteger(precision)) {
+    throw new Error(
+      'нарушены условия: min, precision > 0, max > min, precision - целое число'
+    );
+  }
+  const result = roundTo(min + Math.random() * (max - min), precision);
+  return result > max ? max : result;
 }
 
-getRandomFloatInclusive(0, 1, 1);
+function roundTo(value, precision) {
+  const scalar = 10 ** precision;
+  const result = Math.round(value * scalar) / scalar;
+  return result;
+}
+
+getRandomFloat(0.15, 0.19, 2);
