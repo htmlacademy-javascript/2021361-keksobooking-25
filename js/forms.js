56251925aae5e6e2b850735727da6239d35bb942
Перцев Денис <thech@yandex.ru>
2022-03-20 15:30:41 +0300

    module8-task2

--- js/forms.js
+++ js/forms.js
@@ -1,3 +1,6 @@
+const NOT_FOR_GUESTS = 100;
+const DECIMAL_SYS = 10;
+
 /**
  * Функция возвращает callback-функцию
  * @param {string} methodName - флаг в какое состояние переключить
@@ -33,12 +36,8 @@ export const hideNode = (node) => {
   node.classList.add('visually-hidden');
 };
 
-export const validateAdForm = () => {
-  const form = document.querySelector('.ad-form');
-  const rooms = form.querySelector('#room_number');
-  const capacity = form.querySelector('#capacity');
-
-  const pristine = new Pristine(form, {
+export const validateAdForm = (elms) => {
+  const pristine = new window.Pristine(elms.form, {
     classTo: 'ad-form__element',
     errorClass: 'form__item--invalid',
     successClass: 'form__item--valid',
@@ -47,44 +46,91 @@ export const validateAdForm = () => {
     errorTextClass: 'form__error',
   });
 
-  const removeErrorMsg = () => {
-    const errorMsg = [];
-    errorMsg.push(rooms.parentElement.querySelector('.form__error'));
-    errorMsg.push(capacity.parentElement.querySelector('.form__error'));
-    errorMsg
-      .filter((error) => error !== null)
-      .forEach((error) => (error.textContent = ''));
+  /**
+   * Функция обновляет (очищает) содержимое сообщений об ошибках
+   * @param {Array} errorTextTags - массив тегов с классом '.form__error'
+   */
+  const clearErrorTextTag = (errorTextTags) => {
+    errorTextTags
+      .filter((errorTextTag) => errorTextTag !== null)
+      .forEach((errorTextTag) => (errorTextTag.textContent = ''));
   };
 
-  const getValidateRoomsOrCapacity = () => {
-    const roomsCount = parseInt(rooms.value, 10);
-    const capacityCount = parseInt(capacity.value, 10);
-    removeErrorMsg();
-    return roomsCount === 100
+  const getPriceValidate = () => {
+    const minPrice = parseInt(
+      elms.type[elms.type.selectedIndex].dataset['minprice'],
+      DECIMAL_SYS
+    );
+    const currentPrice = parseInt(elms.price.value, DECIMAL_SYS);
+
+    return currentPrice >= minPrice;
+  };
+
+  const getRoomsOrCapacityValidate = () => {
+    const errorTextTags = [];
+    errorTextTags.push(elms.rooms.parentElement.querySelector('.form__error'));
+    errorTextTags.push(
+      elms.capacity.parentElement.querySelector('.form__error')
+    );
+    clearErrorTextTag(errorTextTags);
+
+    const roomsCount = parseInt(elms.rooms.value, DECIMAL_SYS);
+    const capacityCount = parseInt(elms.capacity.value, DECIMAL_SYS);
+
+    return roomsCount === NOT_FOR_GUESTS
       ? capacityCount === 0
       : capacityCount > 0 && roomsCount >= capacityCount;
   };
+
+  const getPriceErrorMessage = () => 'price error';
 
   const getRoomsErrorMessage = () => 'rooms error';
 
   const getCapacityErrorMessage = () => 'capacity error';
 
+  pristine.addValidator(elms.price, getPriceValidate, getPriceErrorMessage);
+
   pristine.addValidator(
-    rooms,
-    getValidateRoomsOrCapacity,
+    elms.rooms,
+    getRoomsOrCapacityValidate,
     getRoomsErrorMessage
   );
 
   pristine.addValidator(
-    capacity,
-    getValidateRoomsOrCapacity,
+    elms.capacity,
+    getRoomsOrCapacityValidate,
     getCapacityErrorMessage
   );
 
-  form.addEventListener('submit', (evt) => {
+  elms.type.addEventListener('change', () => {
+    elms.price.value = null;
+    elms.price.placeholder =
+      elms.type[elms.type.selectedIndex].dataset['minprice'];
+    const errorTextTags = [];
+    errorTextTags.push(elms.price.parentElement.querySelector('.form__error'));
+    clearErrorTextTag(errorTextTags);
+  });
+
+  elms.form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     if (pristine.validate()) {
-      form.submit();
+      elms.form.submit();
     }
+  });
+};
+
+export const setSelectedPricePlaceholder = (elms) => {
+  elms.price.placeholder =
+    elms.type[elms.type.selectedIndex].dataset['minprice'];
+};
+
+export const setTimeinTimeoutSyncrhro = (elms) => {
+
+  elms.timein.addEventListener('change', () => {
+    elms.timeout.value = elms.timein.value;
+  });
+
+  elms.timeout.addEventListener('change', () => {
+    elms.timein.value = elms.timeout.value;
   });
 };
