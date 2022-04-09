8a0420fbc70d72e62cc83f7efb2e8410c241440c
Перцев Денис <thech@yandex.ru>
2022-04-09 15:06:49 +0300

    module12-task1 фильтр сетится за один цикл записей карты

--- js/main.js
+++ js/main.js
@@ -15,23 +15,30 @@ import {
 disableForm(adFormElements.form);
 disableForm(filtersFormElements.form);
 const mapWhenReady = () => enableForm(adFormElements.form);
-const mapObject = createMap(mapSettings, mapWhenReady, mapCanvas, adFormElements);
+const mapObject = createMap(
+  mapSettings,
+  mapWhenReady,
+  mapCanvas,
+  adFormElements
+);
 initAdForm(adFormElements, filtersFormElements, mapObject);
 setValidateAdForm(adFormElements, filtersFormElements, mapObject);
 createSlider(adFormElements);
 
-getSimilarAds()
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error(`${response.status} ${response.statusText}`);
-  })
-  .then((data) => {
-    setMapEntries(data, mapObject);
-    setFilters(filtersFormElements, mapObject);
-    enableForm(filtersFormElements.form);
-  })
-  .catch((error) => {
-    showMessage(error.message);
-  });
+if (mapObject.map._loaded) {
+  getSimilarAds()
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`${response.status} ${response.statusText}`);
+    })
+    .then((data) => {
+      setMapEntries(data, mapObject);
+      setFilters(filtersFormElements, mapObject);
+      enableForm(filtersFormElements.form);
+    })
+    .catch((error) => {
+      showMessage(error.message);
+    });
+}
